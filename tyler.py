c48f90c06e79b9cd438165e382ea5bec2be4e77e
adisidev <64905594+adisidev@users.noreply.github.com>
2022-04-20 21:24:16 +0800

    Fixed fractal tiling

--- tyler.py
+++ tyler.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import math
 
-img_name = "basic-square.png"
+img_name = "square-loop.png"
 
 inputImage = Image.open(img_name)
 img = inputImage
@@ -11,17 +11,17 @@ output = Image.new(mode="RGB", size=(width * 3, height * 3))
 r = min(width.bit_length(), height.bit_length()) - 1
 
 for i in range(r):
-    for j in range(width, inputImage.width - width, width):
+    for j in range(width, output.width - width, width):
         output.paste(img, (j, height))
-        output.paste(img, (j, inputImage.height - (height * 2)))
+        output.paste(img, (j, output.height - (height * 2)))
 
-    for j in range(height, inputImage.height - height, height):
+    for j in range(height, output.height - height, height):
         output.paste(img, (width, j))
-        output.paste(img, (inputImage.width - (width * 2), j))
+        output.paste(img, (output.width - (width * 2), j))
 
     width //= 2
     height //= 2
 
-    img.resize((width, height))
+    img = img.resize((width, height))
 
 output.save("output-" + img_name)
